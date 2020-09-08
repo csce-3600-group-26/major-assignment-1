c4ff28975951fc51572a96a8e8841419d8e0fdf2
RichardLin1 <RichardLin@my.unt.edu>
2020-09-07 22:07:19 -0500

    Implement Rotate-right

--- src/rotate.c
+++ src/rotate.c
@@ -2,5 +2,8 @@
 
 unsigned long rotate(unsigned long input, unsigned long bit_positions)
 {
-    return 3;
+    unsigned long bit_mask = (1 << bit_positions) - 1;
+    unsigned long masked_input = input & bit_mask;
+    masked_input = masked_input << (8 * sizeof(unsigned long) - bit_positions);
+    return input >> bit_positions | masked_input;
 }
