3c002659d6fb676ac17904d2c6ce8c56cfa85611
hnn0038 <hanguyen6@my.unt.edu>
2020-10-15 06:32:46 -0500

    add endian.c

--- src/endian.c
+++ src/endian.c
@@ -2,5 +2,20 @@
 
 unsigned long endian(unsigned long input)
 {
-    return 2;
+    //assign number address to pointer
+    char *num = (char*)(&input);
+    
+    //swap byte B0 and B3 
+    num[0] = num[0] ^ num[3];
+    num[3] = num[0] ^ num[3];
+    num[0] = num[0] ^ num[3];
+
+    // Swap byte B1 and B2 
+    num[1] = num[1] ^ num[2];
+    num[2] = num[1] ^ num[2];
+    num[1] = num[1] ^ num[2];
+
+    
+    return input;
+    
 }
