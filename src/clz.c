ab7baac6b9e2f0ff5906370c3199976a9a5c9ce3
bld0156 <bld0156@unt.edu>
2020-10-15 21:55:36 -0500

    add clz.c functionality

--- src/clz.c
+++ src/clz.c
@@ -1,6 +1,21 @@
 #include "major1.h"
 
-unsigned long clz(unsigned long input)
+int clz(unsigned long input)
 {
-    return 1;
+    int sum = 0;
+
+    for(int i =31; i >= 0; i--)
+    {
+            int temp = input >> i;
+            if(temp & 1)
+            {
+                    break;
+            }
+            else
+            {
+                    sum++;
+            }
+    }
+
+    return sum;
 }
